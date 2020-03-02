490e9ed8a48fd69492dd48b9177a99ee11ad6254
Timothy Moore <mtimothy984@gmail.com>
2020-03-02 08:25:33 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cache
+
+The loansbot uses a memcached server as a data store for short term completely
+regeneratable information.
+
+This repository is also used to show how we connect to the memcached server in
+python with an ultra simple test.
---
+++ test/test_connection.py
@@ -0,0 +1,44 @@
+"""Connects to the memcached server, setting and deleting one key"""
+from pymemcache.client import base as membase
+import os
+import sys
+import json
+import time
+
+
+def main():
+    memcache_host = os.environ['MEMCACHE_HOST']
+    memcache_port = os.environ['MEMCACHE_PORT']
+    mc_client = membase.Client((memcache_host, memcache_port))
+
+    print('Fetching key test_key:')
+    val = mc_client.get('test_key')
+    if val is not None:
+        print(f'Got weird value! Expected null, got {val}')
+        sys.exit(1)
+
+    original = {'foo': 7}
+    original_json = json.dumps(original)
+    print(f'Setting key test_key to: {original_json} (2 second expire time)')
+    mc_client.set('test_key', original_json, expire=2)
+
+    print('Fetching key test_key:')
+    val = mc_client.get('test_key')
+    if val != original_json:
+        print(f'Got weird value! Expected {original_json}, got {val}')
+        sys.exit(1)
+
+    print('Sleeping 2.2 seconds to allow expiration')
+    time.sleep(2.2)
+
+    print('Fetching key test_key:')
+    val = mc_client.get('test_key')
+    if val is not None:
+        print(f'Got weird value! Expected null, got {val}')
+        sys.exit(1)
+
+    print('All tests passed')
+
+
+if __name__ == '__main__':
+    main()
